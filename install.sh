2eba5bda7de89aa366b3e65f6d222748ec4f8bae
钟梓凌 <ziling.zhong@hand-china.com>
2017-10-01 20:27:03 +0800

    [IMP]修改仓库

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 
 # 配置Git
-curl -o ~/.gitconfig -SL https://raw.githubusercontent.com/TimeBye/Note/master/.gitconfig
+curl -o ~/.gitconfig -SL https://raw.githubusercontent.com/TimeBye/Config/master/.gitconfig
 # 启用“任何来源”
 sudo spctl --master-disable
 # 安装brew
@@ -16,4 +16,4 @@ brew cask install youdaonote qq virtualbox vagrant thunder docker iterm2 iina Ne
 # # 安装oh-my-zsh
 # sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # # 配置zsh
-# curl -o ~/.zshrc -SL https://raw.githubusercontent.com/TimeBye/Note/master/.zshrc
+# curl -o ~/.zshrc -SL https://raw.githubusercontent.com/TimeBye/Config/master/.zshrc
